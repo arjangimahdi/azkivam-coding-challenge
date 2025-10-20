1a863f49b9c5ada1e0e0e5c56e542fe207f957b3
arjangimahdi <github.mahdiarjangi.official@gmail.com>
2025-10-20 22:39:38 +0330

    feat: initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="h-screen w-full bg-zinc-200 p-4">
+    <NuxtPage />
+  </main>
+</template>
---
+++ app/pages/products/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    product list page (PLP)
+  </div>
+</template>
